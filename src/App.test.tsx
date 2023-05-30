f0c6e166f3485015271045ca6757b6381e8ad6c3
trantxo2 <trantxo2@gmail.com>
2023-05-30 18:35:53 +0200

    [1] - Primer test probado

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+it("muestra el parrafo con hola biko", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+  expect(screen.getByText("Hola biko")).toBeInTheDocument();
 });
