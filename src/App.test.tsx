c506eab35f9c737d0091c6815ac724e5bd3068bc
Ivan Sola <113472661+IvanSolaDelgado@users.noreply.github.com>
2023-05-30 19:29:37 +0200

    [1] - Prettier configurado
    Co-authored-by: trantxo2 <trantxo2@gmail.com>

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,9 @@
-import React from "react";
-import { render, screen } from "@testing-library/react";
-import App from "./App";
+import React from 'react'
+import { render, screen } from '@testing-library/react'
+import App from './App'
 
-it("muestra el parrafo con hola biko", () => {
-  render(<App />);
+it('muestra el parrafo con hola biko', () => {
+  render(<App />)
 
-  expect(screen.getByText("Hola biko")).toBeInTheDocument();
-});
+  expect(screen.getByText('Hola biko')).toBeInTheDocument()
+})
--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
-import React from "react";
-import "./App.css";
+import React from 'react'
+import './App.css'
 
 function App() {
   return (
     <div className="App">
-      <p>Hola biko</p>
+      <p>
+        Hola biko Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+        Voluptate illum incidunt fugit maiores possimus neque, exercitationem
+        quia assumenda cumque ipsum officia, rem repudiandae error repellat,
+        corporis laborum aut iure fuga?
+      </p>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
