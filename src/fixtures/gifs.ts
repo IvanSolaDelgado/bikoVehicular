4cdae516b7d9b01e917d43b09777263bdd41c47d
Iván Sola Delgado <ivan@MacBook-Air-de-Ivan.local>
2023-06-09 17:35:39 +0200

    [9] - Refactoriza el handler y crea la carpeta fixtures para el json

---
+++ src/fixtures/gifs.ts
@@ -0,0 +1,10 @@
+export const gifsFixture = [
+  {
+    name: 'perro loco',
+    imgUrl: 'https://media2.giphy.com/media/ouNNlZiPMbv61kzS1M/giphy.gif',
+  },
+  {
+    name: 'gato loco',
+    imgUrl: 'https://media2.giphy.com/media/tRoH9EYLs3lok/giphy.gif',
+  },
+]
--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -1,18 +1,8 @@
 import { rest } from 'msw'
+import { gifsFixture } from '../fixtures/gifs'
 
 export const handlers = [
   rest.get('http://localhost:3000/api/gifs', (req, res, ctx) => {
-    return res(
-      ctx.json([
-        {
-          name: 'perro loco',
-          imgUrl: 'https://media2.giphy.com/media/ouNNlZiPMbv61kzS1M/giphy.gif',
-        },
-        {
-          name: 'gato loco',
-          imgUrl: 'https://media2.giphy.com/media/tRoH9EYLs3lok/giphy.gif',
-        },
-      ]),
-    )
+    return res(ctx.json(gifsFixture))
   }),
 ]
