1accf27d9249590703c0a48309fc3ed444f958a9
Iván Sola Delgado <ivan@MacBook-Air-de-Ivan.local>
2023-06-09 17:08:41 +0200

    [9] - Testea que se muestra el listado de gifs

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,9 +2,10 @@ import { render, screen } from '@testing-library/react'
 import App from './App'
 
 describe('listado de gifs', () => {
-  it('muestra un gif en pantalla con texto alternativo', async () => {
+  it('muestra los gifs en pantalla con texto alternativo', async () => {
     render(<App />)
 
     expect(await screen.findByAltText('perro loco')).toBeVisible()
+    expect(await screen.findByAltText('gato loco')).toBeVisible()
   })
 })
