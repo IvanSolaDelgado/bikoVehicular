b28fa5ddecb92a9e70aad207cdcae53bfcbd657d
Jon <jonguevara@MBP-de-Jon.bikoschool>
2023-06-17 13:51:32 +0200

    [18] - Refactor: back y front vinculado

--- server/src/apiRoutes.ts
+++ server/src/apiRoutes.ts
@@ -2,13 +2,15 @@ import express from 'express'
 import { LowdbSync } from 'lowdb'
 import './app'
 import { DatabaseSchema } from './DatabaseSchema'
+import { mapData } from './domain/GifMap'
 
 export const createRoutes = (db: LowdbSync<DatabaseSchema>) => {
   const apiRoutes = express.Router()
 
   apiRoutes.get('/gifs', (req, res) => {
-    const gifs = db.get('gifs').take(50).value()
-    res.status(200).json(gifs)
+    const gifs = db.get('gifs').take(10).value()
+    res.set('Access-Control-Allow-Origin', '*')
+    res.status(200).json(gifs.map(mapData))
   })
   return apiRoutes
 }
