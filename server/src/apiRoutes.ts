783d44a61e67affc569827eb9457519ce7a57fb1
Jon <jonguevara@MBP-de-Jon.bikoschool>
2023-06-27 18:53:49 +0200

    [14] - cogemos 25 gifs

--- server/src/apiRoutes.ts
+++ server/src/apiRoutes.ts
@@ -8,7 +8,7 @@ export const createRoutes = (db: LowdbSync<DatabaseSchema>) => {
   const apiRoutes = express.Router()
 
   apiRoutes.get('/gifs', (req, res) => {
-    const gifs = db.get('gifs').take(10).value()
+    const gifs = db.get('gifs').take(50).value()
     res.set('Access-Control-Allow-Origin', '*')
     res.status(200).json(gifs.map(mapData))
   })
