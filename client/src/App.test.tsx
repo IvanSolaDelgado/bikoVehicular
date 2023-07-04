5c267d02d5bf1b72e5a27e927e5aca4b669fa824
Iván Sola Delgado <ivan@Air-de-Ivan.bikoschool>
2023-07-04 18:42:41 +0200

    [21] - Feat: primera iteracion del filtrado, hace console log del input cuando clicamos en el boton

--- client/src/App.test.tsx
+++ client/src/App.test.tsx
@@ -35,4 +35,6 @@ describe('listado de gifs', () => {
       await screen.findByText('Sorry, gifs not found :/'),
     ).toBeInTheDocument()
   })
+
+  it('recibe un input al clicar el boton de busqueda', async () => {})
 })
--- client/src/components/Searchbar.tsx
+++ client/src/components/Searchbar.tsx
@@ -1,15 +1,34 @@
 import './Searchbar.css'
 import lupa from '../assets/Lupa.svg'
+import { ChangeEvent, MouseEvent, useState } from 'react'
 
 export const Searchbar = () => {
+  const [query, setQuery] = useState('')
+
+  const handleOnChange = (event: ChangeEvent<HTMLInputElement>) => {
+    console.log(query)
+
+    setQuery(event.target.value)
+  }
+  const handleOnClick = (event: MouseEvent<HTMLElement>) => {
+    event.preventDefault()
+
+    console.log(query)
+  }
   return (
     <form className="searchbar-container">
       <input
         type="search"
         className="searchbar"
         placeholder="¿Que quieres buscar? ¡Encuentralo!"
+        onChange={handleOnChange}
+        value={query}
       />
-      <button className="search-button" aria-label="Search">
+      <button
+        className="search-button"
+        aria-label="Search"
+        onClick={handleOnClick}
+      >
         <img src={lupa} alt="" />
       </button>
     </form>
