42dbc66958211cff443dae464abd8143491b75bd
Ivan Sola <113472661+IvanSolaDelgado@users.noreply.github.com>
2023-07-18 18:58:40 +0200

    [21] - Buscador de gifs
    * [21] - Feat: primera iteracion del filtrado, hace console log del input cuando clicamos en el boton
    
    * [21] - Refactor: invierte la logica de la searchbar y la giflist a Home
    
    * [21] - Feat: filtrado funcionando
    
    ---------
    
    Co-authored-by: Iván Sola Delgado <ivan@Air-de-Ivan.bikoschool>

--- client/src/components/Searchbar.tsx
+++ client/src/components/Searchbar.tsx
@@ -1,15 +1,35 @@
 import './Searchbar.css'
 import lupa from '../assets/Lupa.svg'
+import { ChangeEvent, FC, useState } from 'react'
 
-export const Searchbar = () => {
+interface Props {
+  handleOnClick: (query: string) => void
+}
+
+export const Searchbar: FC<Props> = ({ handleOnClick }) => {
+  const [query, setQuery] = useState('')
+
+  const handleOnChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setQuery(event.target.value)
+  }
+
   return (
     <form className="searchbar-container">
       <input
         type="search"
         className="searchbar"
         placeholder="¿Que quieres buscar? ¡Encuentralo!"
+        onChange={handleOnChange}
+        value={query}
       />
-      <button className="search-button" aria-label="Search">
+      <button
+        className="search-button"
+        aria-label="Search"
+        onClick={(e) => {
+          e.preventDefault()
+          handleOnClick(query)
+        }}
+      >
         <img src={lupa} alt="" />
       </button>
     </form>
--- client/src/components/pages/Home.tsx
+++ client/src/components/pages/Home.tsx
@@ -1,18 +1,54 @@
+import { useEffect, useState } from 'react'
+import { Gif } from '../../domain/Gif'
+import { gifService } from '../../domain/GifService'
 import { GifList } from '../GifList'
 import { Navbar } from '../Navbar'
 import { Searchbar } from '../Searchbar'
 import './Home.css'
 
 export const Home = () => {
+  const [gifs, setGifs] = useState<Gif[]>([])
+  const [isLoading, setIsLoading] = useState(false)
+  const [filteredGifs, setFilteredGifs] = useState<Gif[]>([])
+
+  const getGifs = async () => {
+    setIsLoading(true)
+
+    const gifs = await gifService.getGifs()
+    setGifs(gifs)
+
+    setIsLoading(false)
+  }
+
+  useEffect(() => {
+    getGifs()
+  }, [])
+
+  useEffect(() => {
+    setFilteredGifs(gifs)
+  }, [gifs])
+
+  const filterGifs = (query: string) => {
+    const filterGifs = gifs.filter((gif) =>
+      gif.name.toLowerCase().includes(query.toLowerCase()),
+    )
+
+    setFilteredGifs(filterGifs)
+  }
+
+  if (isLoading) return <p>Loading...</p>
+
+  if (gifs.length === 0) return <p>Sorry, gifs not found :/</p>
+
   return (
     <section className="home-container">
       <section>
         <header>
           <Navbar />
-          <Searchbar />
+          <Searchbar handleOnClick={filterGifs} />
         </header>
         <main>
-          <GifList />
+          <GifList gifs={filteredGifs} />
         </main>
       </section>
     </section>
