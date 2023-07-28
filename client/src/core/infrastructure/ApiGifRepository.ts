06b727f310e32617c0d4f00d1e4864bbb9024c4d
trantxo2 <trantxo2@gmail.com>
2023-07-28 18:12:13 +0200

    creacion de la interfaz de dominio

--- client/src/core/infrastructure/ApiGifRepository.ts
+++ client/src/core/infrastructure/ApiGifRepository.ts
@@ -1,9 +1,14 @@
 import { Gif } from '../domain/Gif'
+import { GifRepository } from '../domain/GifRepository'
 
 const BASE_URL = 'http://localhost:3000/api/gifs'
 
-export const getGifListDetails = async (): Promise<Gif[]> => {
+const getGifListDetails = async (repository: GifRepository): Promise<Gif[]> => {
   const response = await fetch(BASE_URL)
   const data = await response.json()
   return data
+}
+
+export const apiGifRepository = {
+  getGifListDetails,
 }
